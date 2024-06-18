6f4d50c2e3f7d2b4d5345dc90b964604ba0827e8
Sinan Fischer <sinanfischer@gmail.com>
2024-06-19 00:16:16 +0200

    ini t

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'darkBlue': '#262E38', 
+        'lightGray': '#969FAD', 
+        'orange': '#FC7614', 
+        'white': '#FFFFFF',
+        'veryDarkBlue': '#131518',
+        'blackG': '#222A33'
+    },
+  },
+  plugins: [],
+}
+}
